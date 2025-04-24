2840362a35e3d83233702cc6e7d532928bbe32f6
sofigo1010 <110646163+sofigo1010@users.noreply.github.com>
2025-04-24 00:48:20 -0600

    feat: add routes for testimonials, stats, and popular dishes in API

--- restaurante-api/routes/api.js
+++ restaurante-api/routes/api.js
@@ -7,5 +7,9 @@ router.use('/restaurantes',  require('./restaurantes'));
 router.use('/articulos',     require('./articulos'));
 router.use('/ordenes',       require('./ordenes'));
 router.use('/resenas',       require('./resenas'));
+router.use('/testimonials', require('./testimonials')); 
+router.use('/stats',        require('./stats')); 
+router.use('/popular-dishes',  require('./popularDishes')); 
+
 
 module.exports = router;
